f168ffa0258c2d0c5f4e21633ef5399fa3c10ace
Willian Rafael Veres <willianrafaelveres14@gmail.com>
2021-08-26 21:37:52 -0300

    Cópia Pastas

---
+++ Aula 3/MostrarNomeEData/Program.cs
@@ -0,0 +1,24 @@
+﻿//Willian Rafael Veres - 23/08/2021
+using System;
+
+namespace MostrarNomeEData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sobrenome = "";
+            string dataNascimento = "";
+
+            Console.Clear();
+
+            Console.WriteLine("Mostrar sobrenome e data de nascimento!");
+            Console.WriteLine("Digite seu sobrenome:");
+            sobrenome = Console.ReadLine();
+            Console.WriteLine("Digite sua data de nascimento: ex:00/00/0000");
+            dataNascimento = Console.ReadLine();
+
+            Console.WriteLine($"Seu sobrenome é {sobrenome} e sua data de nascimento é {dataNascimento}!");
+        }
+    }
+}

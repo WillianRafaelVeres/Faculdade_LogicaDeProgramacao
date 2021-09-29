5ccfe8bbc02c767b7b9c86e2811543858bf9d8dc
Willian Rafael Veres <willianrafaelveres14@gmail.com>
2021-09-28 23:14:33 -0300

    28_09_2021

--- Aula8/Exercicio3/Program.cs
+++ Aula8/Exercicio3/Program.cs
@@ -6,56 +6,53 @@ namespace Exercicio3
     {
         static void Main(string[] args)
         {
-            int[] notas;
-            int[] notasComMedia4;
-            int[] notasComMedia6;
-            int[] soma;
-            int contador = 1;
-            int contador2 = 0;
-            int notaFinal = 0;
-
             Console.Clear();
 
-            while(contador < 7){
-                Console.WriteLine($"Digite a nota {contador}");
-                notas.add(Convert.ToInt32(Console.ReadLine()));
-                contador++;
+            Console.WriteLine("Programa de média");
+
+            double nota1; // Variável da nota 1
+            double nota2; // Variável da nota 2
+            double nota3; // Variável da nota 3
+            double nota4; // Variável da nota 3
+            double nota5; // Variável da nota 3
+            double nota6; // Variável da nota 3
+            double notaFinal;
+
+            nota1 = LerNota(1); // Chama a função para ler nota e armazena em nota1
+            nota2 = LerNota(2); // Chama a função para ler nota e armazena em nota2
+            nota3 = LerNota(3); // Chama a função para ler nota e armazena em nota3
+            nota4 = LerNota(4); // Chama a função para ler nota e armazena em nota4
+            nota5 = LerNota(5); // Chama a função para ler nota e armazena em nota5
+            nota6 = LerNota(6); // Chama a função para ler nota e armazena em nota6
+
+            notaFinal = MediaNumeros(nota1, nota2, nota3, nota4, nota5, nota6); // Chama a função para a média e passa os parâmentros
+            VerificacaoDeSituacao(notaFinal);
+        }
+
+        static double LerNota(double numeroNota) // Função para ler nota
+        {   
+            string dadoRecebido = ""; // Variável do dado digitado
+            Console.WriteLine($"Por favor, Digite a {numeroNota}º nota: "); // Mostra a mensagem solicitando a nota
+            dadoRecebido = Console.ReadLine();  // Armazena o dado digitado em dadoRecebido
+            return Convert.ToDouble(dadoRecebido); // Retorna o dadoRecebido convertido para double
+        }
+
+        static double MediaNumeros(double nota1, double nota2, double nota3, double nota4, double nota5, double nota6) // Função para fazer a média ponderada de 6 números
+        {
+             return (((nota1 * 6 + nota2 * 4)/10) + ((nota3 * 6 + nota4 * 4)/10) + ((nota5 * 6 + nota6 * 4)/10)) / 3;
+        }
+
+        static void VerificacaoDeSituacao(double notaFinal){
+
+            Console.WriteLine($"A sua média final é {Math.Round(notaFinal,2)}!");
+
+            if(notaFinal >= 6){
+                Console.WriteLine("Sua situação é Aprovado");
             }
 
-            while(contador2 < 6){
-                
-                if(contador%2 == 0){
-                    notasComMedia4.add(notas[contador2]);
-                }
-
-                else{
-                    notasComMedia6.add(notas[contador2]);
-                }
-                contador2++;
+            else{
+                Console.WriteLine("Sua situação é Reprovado");
             }
-
-            while(contador2 < 3){
-                soma.add((notasComMedia4[contador2]*4 + notasComMedia6[contador2]*6)/10);
-            }
-
-            foreach(int nota in soma){
-                notaFinal += nota;
-            }
-
-            notaFinal = notaFinal / 3;
-
-            Console.WriteLine(notaFinal);
-
-
-
-
-
-
-            foreach( int nota in notas){
-                
-            }
-
-
         }
     }
 }
--- Aula8/Exercicio4/Program.cs
+++ Aula8/Exercicio4/Program.cs
@@ -6,7 +6,54 @@ namespace Exercicio4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+
+            Console.WriteLine("Programa de identificação de categoria!");
+            IdentificacaoDeCategoria();
+
+        }
+
+        static void IdentificacaoDeCategoria(){
+
+            int idade;
+            string continuar;
+            string respostaParaComparar;
+
+            Console.Clear();
+
+            Console.Write("Digite sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"A idade digitada foi {idade} anos!");
+
+            if(idade >= 5 && idade <= 7){
+                Console.WriteLine("Esta idade se encaixa na categoria: INFANTIL A !");
+            }
+            else if(idade >= 8 && idade <= 10){
+                Console.WriteLine("Esta idade se encaixa na categoria: INFANTIL B !");
+            }
+            else if(idade >= 11 && idade <= 13){
+                Console.WriteLine("Esta idade se encaixa na categoria: JUVENIL A !");
+            }
+            else if(idade >= 14 && idade <= 17){
+                Console.WriteLine("Esta idade se encaixa na categoria: JUVENIL B !");
+            }
+            else if(idade >= 18 && idade <= 65){
+                Console.WriteLine("Esta idade se encaixa na categoria: SÊNIOR !");
+            }
+            else{
+                Console.WriteLine("Está idade não se encaixa em nenhuma das nossas categorias!");
+            }
+
+            Console.WriteLine("\nDeseja digitar uma nova idade?");
+            Console.WriteLine("Digite apenas Sim ou Não");
+            continuar = Console.ReadLine();
+            respostaParaComparar = continuar.ToUpper();
+
+            if(respostaParaComparar == "SIM"){
+                IdentificacaoDeCategoria();
+            }
+
         }
     }
 }

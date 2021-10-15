a100ab423fd01fb240962fe2996769019ba3414f
Willian Rafael Veres <willianrafaelveres14@gmail.com>
2021-10-14 21:46:42 -0300

    13_10_21

--- DesafiosM2/Exercicio1/Program.cs
+++ DesafiosM2/Exercicio1/Program.cs
@@ -6,7 +6,57 @@ namespace Exercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double mediaSaldo;
+
+            Console.Clear();
+
+            Console.WriteLine("Calculo de crédito");
+
+            mediaSaldo = LerMediaSaldo();
+            CalculaCredito(mediaSaldo);
+
+        }
+
+        static double LerMediaSaldo(){
+            Console.Write("Digite o valor da média anual do saldo:");
+            return double.Parse(Console.ReadLine());
+        }
+
+        static void CalculaCredito(double saldo){
+
+            double valorCredito = 0;
+            double valorTotal = 0;
+
+            if (saldo <= 200)
+            {
+                Console.WriteLine("\nSeu saldo não se encaixa em nenhum crédito especial!");
+                valorCredito = 0;
+            }
+
+            else if (saldo <= 400)
+            {
+                Console.WriteLine("\nSeu saldo se encaixa em 20% de crédito especial!");
+                valorCredito = saldo * 20 / 100;
+            }
+
+            else if (saldo <= 600)
+            {
+                Console.WriteLine("\nSeu saldo se encaixa em 30% de crédito especial!");
+                valorCredito = saldo * 30 / 100;
+            }
+
+            else if (saldo > 600)
+            {
+                Console.WriteLine("\nSeu saldo se encaixa em 40% de crédito especial!");
+                valorCredito = saldo * 40 / 100;
+            }
+
+            valorTotal = valorCredito + saldo;
+
+            Console.WriteLine($"Valor saldo: R${Math.Round(saldo,2)}");
+            Console.WriteLine($"Valor do crédito especial: R${Math.Round(valorCredito)}");
+            Console.WriteLine($"Valor total: R${Math.Round(valorTotal,2)}\n");
+
         }
     }
 }
--- DesafiosM2/Exercicio2/Program.cs
+++ DesafiosM2/Exercicio2/Program.cs
@@ -6,7 +6,40 @@ namespace Exercicio2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            LeituraEValidacao();
+            
         }
+
+        static string LeituraEValidacao(){
+
+            string codigo;
+
+            Console.Write("Digite o código:");
+            codigo = Console.ReadLine();
+
+            if(codigo == "ABCD" || codigo == "XYPK" || codigo == "KLMP" || codigo == "QRST"){
+                Console.WriteLine("Tudo certo");
+                return codigo;
+            }
+
+            else{
+
+                Console.WriteLine("Código inválido! Tente novamente! \n");
+                LeituraEValidacao();
+            }
+
+            /*02) Um vendedor precisa de um programa que calcule o preço total devido por um cliente. O
+            algoritmo deve receber o código de um produto e a quantidade comprada e calcular o preço
+                total, usando a tabela abaixo. Mostre uma mensagem no caso de código inválido. Código
+            Preço Unitário 'ABCD' R$100 'XYPK' R$ 200 'KLMP' R$400 'QRST' R$ 1000.
+            Código Valor
+            ABCD 100
+            XYPK 200
+            KLMP 400
+            QRST 1000*/
+
+        }
+
     }
 }

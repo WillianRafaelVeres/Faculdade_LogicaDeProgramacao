467e72661e836716bdb5defc4a6b1cf294707d15
Willian Rafael Veres <willianrafaelveres14@gmail.com>
2021-11-16 13:45:53 -0300

    15_11_21

--- DesafiosM3/DesafioM3/Cilindro.cs
+++ DesafiosM3/DesafioM3/Cilindro.cs
@@ -2,21 +2,40 @@ using System;
 
 namespace DesafioM3
 {
-    class Cilindro
+    public class Cilindro
     {
-        public static double raio;
-        public static double altura;
+        private double raio;
+        private double altura;
+        private double volume;
 
-        public static double CalculaVolume(double r, double h)
+        private double areaDaBase;
+        private double areaLateal;
+        private double areaTotal;
+        private double perimetro;
+
+        public Cilindro(double r, double a, double v){
+            
+            raio = r;
+            altura = a;
+            volume = v;
+
+            areaDaBase = null;
+            areaLateal = null;
+            areaTotal = null;
+            perimetro = null;
+
+        }
+
+        public void CalculaVolume()
         {
-            return (Math.PI * (r * r) * h);
+            areaDaBase = (Math.PI * (r * r) * h);
         }
 
-        public static double CalculaAreaDaBase(double r){
-            return (Math.PI * (r * r));
+        public void CalculaAreaDaBase(){
+            areaLateal = (Math.PI * (r * r));
         }
 
-        public static double CalculaAreaLateral(double r, double h){
+        public void CalculaAreaLateral(){
             return (2 * Math.PI * r * h);
         }
 
@@ -27,5 +46,36 @@ namespace DesafioM3
         public static double CalculaPerimetro(double r){
             return (2 * Math.PI * r);
         }
+    }
+}
+
+//m4rc3lo - 12/11/2021
+using System; // inclusão de Math / Console 
+namespace M3
+{
+
+        // procedimento, calcula a área da esfera
+        //armazena na variável de instância
+        public void calcula_area()
+        {
+            area = 4 * Math.PI * Math.Pow(raio, 2);
+        }
+        //procedimento, calcula o volume da esfera
+        //armazena na variável de isntância
+        public void calcula_volume()
+        {
+            volume = (4 * Math.PI  * Math.Pow(raio, 3)) / 3;
+        }
+
+        // funções para acessar o valor das variáveis de instância
+        public double  get_area()
+        {
+            return area; // retorna o valor da área 
+        }
+        public double get_volume()
+        {
+            return volume; //retorna o valor do volume
+        }
+        ///---------------------------------------------------------------
     }
 }
